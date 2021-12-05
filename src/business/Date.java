d57caf1f0d0eebb940cf00d92ff1439109bed0fb
ibrahim29asad <72579268+ibrahim29asad@users.noreply.github.com>
2021-12-05 14:36:26 -0700

    Add files via upload
    Need to add singleton

---
+++ src/business/Date.java
@@ -0,0 +1,49 @@
+
+public class Date {
+	
+	private String day;
+	private String month;
+	private String year;	
+	
+	
+		
+	public Date(String d, String m, String y){
+		day = d;
+		month = m;
+		year = y;
+	}
+	
+	
+	
+	
+	public void setYear(String a){
+		year = a;
+	}
+	
+	public String getYear(){
+		return year;
+	}
+		
+	
+	public void setMonth(String a){
+		month = a;
+	}
+	
+	public String getMonth(){
+		return month;
+	}
+	
+	
+	public void setDay(String a){
+		day = a;
+	}
+	
+	public String getDay(){
+		return day;
+	}
+	
+	
+	
+	
+	
+}
