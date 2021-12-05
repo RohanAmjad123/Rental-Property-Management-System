9ad0223a99debc05e6ba0b30faf5afb5131b96ff
AzlanAmjad <Azlanamjad_123@hotmail.com>
2021-12-05 02:53:28 -0700

    Added Constructors

--- src/presentation/guicomponents/Frontend.java
+++ src/presentation/guicomponents/Frontend.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 
 /**
  * Class Frontend
+ * 
  * @since December 4th, 2021
  * @author Azlan Amjad
  * @author Rohan Amjad
@@ -12,5 +13,43 @@ import javax.swing.*;
  * @version 1.0
  */
 public class Frontend extends JFrame {
+    private SignupGUI signup;
+    private LoginGUI login;
+    private RegisterPropertyGUI registerProperty;
+    private ManageSubscriptionsGUI manageSubscriptions;
+    private AddSearchCriteriaFormGUI addSearchCriteriaForm;
+    private ManageLandlordPropertiesGUI manageLandlordProperties;
+    private PayFeeFormGUI payFeeForm;
+    private ManageManagerPropertiesGUI manageManagerProperties;
+    private ChangeFeesGUI changeFees;
+    private ViewCompanyDatabaseGUI viewCompanyDatabase;
+    private PeriodicalReportFormGUI periodicalReportForm;
+    private DashboardGUI dashboard;
+    private PropertyViewGUI propertyView;
 
+    /**
+     * class Frontend constructor, used by class Backend to construct its view object.
+     */
+    public Frontend() {
+        // construct parent class JFrame
+        super("Rental Property Management System");
+
+        // construct all GUI JPanels
+        signup = new SignupGUI(this);
+        login = new LoginGUI(this);
+        registerProperty = new RegisterPropertyGUI(this);
+        manageSubscriptions = new ManageSubscriptionsGUI(this);
+        addSearchCriteriaForm = new AddSearchCriteriaFormGUI(this);
+        manageLandlordProperties = new ManageLandlordPropertiesGUI(this);
+        payFeeForm = new PayFeeFormGUI(this);
+        manageManagerProperties = new ManageManagerPropertiesGUI(this);
+        changeFees = new ChangeFeesGUI(this);
+        viewCompanyDatabase = new ViewCompanyDatabaseGUI(this);
+        periodicalReportForm = new PeriodicalReportFormGUI(this);
+        dashboard = new DashboardGUI(this);
+        propertyView = new PropertyViewGUI(this);
+
+        // set DashboardGUI on JFrame
+        this.add(dashboard);
+    }
 }
