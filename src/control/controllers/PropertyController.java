34b48d086dd5f11ce3319c623cf0721ef149ee0a
Rohan Amjad <rohanamjad123@gmail.com>
2021-12-04 19:34:08 -0700

    added controller classes

---
+++ src/control/controllers/PropertyController.java
@@ -0,0 +1,14 @@
+package control.controllers;
+
+/**
+ * Class PropertyController
+ * @since December 4th, 2021
+ * @author Azlan Amjad
+ * @author Rohan Amjad
+ * @author Gibran Akmal
+ * @author Ibrahim Asad
+ * @version 1.0
+ */
+public class PropertyController {
+    
+}
