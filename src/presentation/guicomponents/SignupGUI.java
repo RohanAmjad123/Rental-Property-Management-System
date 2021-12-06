e1c4b9a3e96e691efc1a86810861795c36878e7b
AzlanAmjad <Azlanamjad_123@hotmail.com>
2021-12-06 04:18:10 -0700

    added successPage() and errorPage()

--- src/presentation/guicomponents/SignupGUI.java
+++ src/presentation/guicomponents/SignupGUI.java
@@ -17,7 +17,7 @@ public class SignupGUI extends JPanel {
     private Controller signupListener;
     private Frontend view;
 
-    // Variables declaration - do not modify                     
+    // Variables declaration - do not modify
     private javax.swing.JLabel copyrightLabel;
     private javax.swing.JButton dashboardButton;
     private javax.swing.JButton landlordButton;
@@ -28,9 +28,9 @@ public class SignupGUI extends JPanel {
     private javax.swing.JButton renterButton;
     private javax.swing.JLabel renterLabel;
     private javax.swing.JLabel userChoiceLabel;
-    // End of variables declaration 
+    // End of variables declaration
 
-    // Variables declaration - do not modify                     
+    // Variables declaration - do not modify
     private javax.swing.JLabel emailLabel;
     private javax.swing.JTextField emailText;
     private javax.swing.JLabel firstNameLabel;
@@ -41,11 +41,27 @@ public class SignupGUI extends JPanel {
     private javax.swing.JPasswordField passwordText;
     private javax.swing.JLabel renterSignupLabel;
     private javax.swing.JButton signupButton;
-    // End of variables declaration 
+    // End of variables declaration
+
+    // Variables declaration - do not modify
+    private javax.swing.JLabel managerKeyLabel;
+    private javax.swing.JPasswordField managerKeyText;
+    private javax.swing.JLabel managerSignupLabel;
+    // End of variables declaration
 
     // Variables declaration - do not modify
     private javax.swing.JLabel landordSignupLabel;
-    // End of variables declaration 
+    // End of variables declaration
+
+    // Variables declaration - do not modify
+    private javax.swing.JButton finishButton;
+    private javax.swing.JLabel signupSuccessfulLabel;
+    // End of variables declaration
+
+    // Variables declaration - do not modify
+    private javax.swing.JLabel signupUnsuccessfulLabel;
+    private javax.swing.JButton tryAgainButton;
+    // End of variables declaration
 
     public SignupGUI(Frontend view) {
         // assign view reference
@@ -58,6 +74,8 @@ public class SignupGUI extends JPanel {
     }
 
     private void userChoice() {
+        removeAll();
+
         rentSlowerLabel = new javax.swing.JLabel();
         managerLabel = new javax.swing.JLabel();
         userChoiceLabel = new javax.swing.JLabel();
@@ -139,7 +157,7 @@ public class SignupGUI extends JPanel {
         renterButton.setText("Renter");
         renterButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                
+
             }
         });
         add(renterButton);
@@ -156,9 +174,14 @@ public class SignupGUI extends JPanel {
         });
         add(managerButton);
         managerButton.setBounds(150, 370, 140, 50);
+
+        revalidate();
+        repaint();
     }
 
     private void renterSignup() {
+        removeAll();
+
         rentSlowerLabel = new javax.swing.JLabel();
         passwordLabel = new javax.swing.JLabel();
         renterSignupLabel = new javax.swing.JLabel();
@@ -276,9 +299,14 @@ public class SignupGUI extends JPanel {
         });
         add(dashboardButton);
         dashboardButton.setBounds(370, 20, 50, 40);
+
+        revalidate();
+        repaint();
     }
 
     private void landlordSignup() {
+        removeAll();
+
         rentSlowerLabel = new javax.swing.JLabel();
         passwordLabel = new javax.swing.JLabel();
         landordSignupLabel = new javax.swing.JLabel();
@@ -396,7 +424,236 @@ public class SignupGUI extends JPanel {
         });
         add(dashboardButton);
         dashboardButton.setBounds(370, 20, 50, 40);
+
+        revalidate();
+        repaint();
     }
 
+    private void managerSignup() {
+        removeAll();
 
+        rentSlowerLabel = new javax.swing.JLabel();
+        managerKeyLabel = new javax.swing.JLabel();
+        managerSignupLabel = new javax.swing.JLabel();
+        firstNameLabel = new javax.swing.JLabel();
+        lastNameLabel = new javax.swing.JLabel();
+        signupButton = new javax.swing.JButton();
+        copyrightLabel = new javax.swing.JLabel();
+        emailLabel = new javax.swing.JLabel();
+        emailText = new javax.swing.JTextField();
+        firstNameText = new javax.swing.JTextField();
+        lastNameText = new javax.swing.JTextField();
+        managerKeyText = new javax.swing.JPasswordField();
+        dashboardButton = new javax.swing.JButton();
+        passwordLabel = new javax.swing.JLabel();
+        passwordText = new javax.swing.JPasswordField();
+
+        setBackground(java.awt.Color.darkGray);
+        setPreferredSize(new java.awt.Dimension(436, 491));
+        setLayout(null);
+
+        rentSlowerLabel.setFont(new java.awt.Font("Heiti TC", 1, 48)); // NOI18N
+        rentSlowerLabel.setForeground(new java.awt.Color(255, 255, 255));
+        rentSlowerLabel.setText("RentSlower");
+        add(rentSlowerLabel);
+        rentSlowerLabel.setBounds(70, 20, 290, 70);
+
+        managerKeyLabel.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        managerKeyLabel.setForeground(new java.awt.Color(255, 255, 255));
+        managerKeyLabel.setText("Manager Key");
+        add(managerKeyLabel);
+        managerKeyLabel.setBounds(60, 380, 80, 20);
+
+        managerSignupLabel.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        managerSignupLabel.setForeground(new java.awt.Color(255, 255, 255));
+        managerSignupLabel.setText("Manager Signup");
+        add(managerSignupLabel);
+        managerSignupLabel.setBounds(130, 90, 180, 30);
+
+        firstNameLabel.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        firstNameLabel.setForeground(new java.awt.Color(255, 255, 255));
+        firstNameLabel.setText("First Name");
+        add(firstNameLabel);
+        firstNameLabel.setBounds(60, 140, 120, 20);
+
+        lastNameLabel.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        lastNameLabel.setForeground(new java.awt.Color(255, 255, 255));
+        lastNameLabel.setText("Last Name");
+        add(lastNameLabel);
+        lastNameLabel.setBounds(60, 200, 130, 20);
+
+        signupButton.setBackground(new java.awt.Color(255, 255, 255));
+        signupButton.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        signupButton.setForeground(new java.awt.Color(0, 0, 0));
+        signupButton.setText("Signup");
+        signupButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+
+            }
+        });
+        add(signupButton);
+        signupButton.setBounds(280, 420, 120, 40);
+
+        copyrightLabel.setForeground(new java.awt.Color(255, 255, 255));
+        copyrightLabel.setText("© 2021 RentSlower Team");
+        add(copyrightLabel);
+        copyrightLabel.setBounds(20, 460, 170, 20);
+
+        emailLabel.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        emailLabel.setForeground(new java.awt.Color(255, 255, 255));
+        emailLabel.setText("Email");
+        add(emailLabel);
+        emailLabel.setBounds(60, 260, 80, 20);
+
+        emailText.setBackground(java.awt.Color.black);
+        emailText.setForeground(new java.awt.Color(255, 255, 255));
+        emailText.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+
+            }
+        });
+        add(emailText);
+        emailText.setBounds(50, 290, 270, 18);
+
+        firstNameText.setBackground(java.awt.Color.black);
+        firstNameText.setForeground(new java.awt.Color(255, 255, 255));
+        firstNameText.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+
+            }
+        });
+        add(firstNameText);
+        firstNameText.setBounds(50, 170, 270, 18);
+
+        lastNameText.setBackground(java.awt.Color.black);
+        lastNameText.setForeground(new java.awt.Color(255, 255, 255));
+        lastNameText.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+
+            }
+        });
+        add(lastNameText);
+        lastNameText.setBounds(50, 230, 270, 18);
+
+        managerKeyText.setBackground(java.awt.Color.black);
+        managerKeyText.setForeground(new java.awt.Color(255, 255, 255));
+        add(managerKeyText);
+        managerKeyText.setBounds(50, 410, 170, 20);
+
+        dashboardButton.setBackground(new java.awt.Color(255, 255, 255));
+        dashboardButton.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        dashboardButton.setForeground(new java.awt.Color(0, 0, 0));
+        dashboardButton.setText("X");
+        dashboardButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+
+            }
+        });
+        add(dashboardButton);
+        dashboardButton.setBounds(370, 20, 50, 40);
+
+        passwordLabel.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        passwordLabel.setForeground(new java.awt.Color(255, 255, 255));
+        passwordLabel.setText("Password");
+        add(passwordLabel);
+        passwordLabel.setBounds(60, 320, 80, 20);
+
+        passwordText.setBackground(java.awt.Color.black);
+        passwordText.setForeground(new java.awt.Color(255, 255, 255));
+        add(passwordText);
+        passwordText.setBounds(50, 350, 270, 20);
+
+        revalidate();
+        repaint();
+    }
+
+    private void successPage() {
+        removeAll();
+
+        rentSlowerLabel = new javax.swing.JLabel();
+        signupSuccessfulLabel = new javax.swing.JLabel();
+        finishButton = new javax.swing.JButton();
+        copyrightLabel = new javax.swing.JLabel();
+
+        setBackground(java.awt.Color.darkGray);
+        setPreferredSize(new java.awt.Dimension(436, 491));
+        setLayout(null);
+
+        rentSlowerLabel.setFont(new java.awt.Font("Heiti TC", 1, 48)); // NOI18N
+        rentSlowerLabel.setForeground(new java.awt.Color(255, 255, 255));
+        rentSlowerLabel.setText("RentSlower");
+        add(rentSlowerLabel);
+        rentSlowerLabel.setBounds(90, 20, 290, 70);
+
+        signupSuccessfulLabel.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        signupSuccessfulLabel.setForeground(new java.awt.Color(255, 255, 255));
+        signupSuccessfulLabel.setText("Signup Successful! ");
+        add(signupSuccessfulLabel);
+        signupSuccessfulLabel.setBounds(120, 170, 210, 40);
+
+        finishButton.setBackground(new java.awt.Color(255, 255, 255));
+        finishButton.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        finishButton.setForeground(new java.awt.Color(0, 0, 0));
+        finishButton.setText("Finish");
+        finishButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+
+            }
+        });
+        add(finishButton);
+        finishButton.setBounds(160, 240, 120, 40);
+
+        copyrightLabel.setForeground(new java.awt.Color(255, 255, 255));
+        copyrightLabel.setText("© 2021 RentSlower Team");
+        add(copyrightLabel);
+        copyrightLabel.setBounds(20, 460, 170, 20);
+
+        revalidate();
+        repaint();
+    }
+
+    private void errorPage() {
+        removeAll();
+
+        rentSlowerLabel = new javax.swing.JLabel();
+        signupUnsuccessfulLabel = new javax.swing.JLabel();
+        tryAgainButton = new javax.swing.JButton();
+        copyrightLabel = new javax.swing.JLabel();
+
+        setBackground(java.awt.Color.darkGray);
+        setPreferredSize(new java.awt.Dimension(436, 491));
+        setLayout(null);
+
+        rentSlowerLabel.setFont(new java.awt.Font("Heiti TC", 1, 48)); // NOI18N
+        rentSlowerLabel.setForeground(new java.awt.Color(255, 255, 255));
+        rentSlowerLabel.setText("RentSlower");
+        add(rentSlowerLabel);
+        rentSlowerLabel.setBounds(90, 20, 290, 70);
+
+        signupUnsuccessfulLabel.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        signupUnsuccessfulLabel.setForeground(new java.awt.Color(255, 255, 255));
+        signupUnsuccessfulLabel.setText("Signup Unsuccessful!");
+        add(signupUnsuccessfulLabel);
+        signupUnsuccessfulLabel.setBounds(110, 170, 230, 40);
+
+        tryAgainButton.setBackground(new java.awt.Color(255, 255, 255));
+        tryAgainButton.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        tryAgainButton.setForeground(new java.awt.Color(0, 0, 0));
+        tryAgainButton.setText("Try Again");
+        tryAgainButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+
+            }
+        });
+        add(tryAgainButton);
+        tryAgainButton.setBounds(160, 240, 120, 40);
+
+        copyrightLabel.setForeground(new java.awt.Color(255, 255, 255));
+        copyrightLabel.setText("© 2021 RentSlower Team");
+        add(copyrightLabel);
+        copyrightLabel.setBounds(20, 460, 170, 20);
+
+        revalidate();
+        repaint();
+    }
 }
