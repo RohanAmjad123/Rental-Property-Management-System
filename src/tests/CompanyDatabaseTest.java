69758563ec5029c0e3fdf065e49dc03a65733445
Rohan Amjad <rohanamjad123@gmail.com>
2021-12-08 20:25:24 -0700

    ignore *.class files

--- src/tests/CompanyDatabaseTest.java
+++ src/tests/CompanyDatabaseTest.java
@@ -1,9 +1,6 @@
 package tests;
 
-import static org.junit.Assert.*;
 import org.junit.*;
-import datasource.companydatabase.*;
-import java.sql.*;
 import control.controllers.*;
 
 public class CompanyDatabaseTest {
