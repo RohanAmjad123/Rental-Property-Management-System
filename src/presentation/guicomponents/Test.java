a395c2edb204a4521968bc0eea7e3d2556dffacd
AzlanAmjad <Azlanamjad_123@hotmail.com>
2021-12-09 04:10:58 -0700

    updated Frontend

--- src/presentation/guicomponents/Test.java
+++ src/presentation/guicomponents/Test.java
@@ -4,5 +4,21 @@ public class Test {
     public static void main(String args[]) {
         Frontend view = new Frontend();
         view.setVisible(true);
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+
+        view.signup();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+
+        view.registerProperty();
     }
 }
