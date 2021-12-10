b0562b14985e00bf568b75d8b5486248fe83b948
AzlanAmjad <Azlanamjad_123@hotmail.com>
2021-12-09 18:42:15 -0700

    implemented toString() for User

--- src/presentation/guicomponents/Frontend.java
+++ src/presentation/guicomponents/Frontend.java
@@ -47,7 +47,7 @@ public class Frontend extends JFrame {
         propertyView = new PropertyViewGUI(this);
         dashboard = new DashboardGUI(this);
 
-        this.manageSubscriptions();
+        this.dashboard();
     }
 
     // methods for changing panel on frame
