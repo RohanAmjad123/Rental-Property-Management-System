f09ded501540908e86d0fce0b0560251180cd636
ibrahim29asad <72579268+ibrahim29asad@users.noreply.github.com>
2021-12-10 01:57:03 -0700

    Add files via upload

--- src/business/businesslogic/Address.java
+++ src/business/businesslogic/Address.java
@@ -19,6 +19,19 @@ public class Address {
 	private String stateProvince;
 	private String country;
 
+    /**
+     * Address constuctor, Initializes the values of sn, pc, cq, ct
+     * sp, and cou with streetName, postalCode, cityQuadrant, city, 
+     * stateProvince, and country respectivly and throws an IllegalArgumentException 
+     * if values are not valid such as having a number in country.
+     * @param sn parameter is a Type String and represents streetName 
+     * @param pc parameter is a Type String and represents postalCode 
+     * @param cq parameter is a Type String and represents cityQuadrant 
+     * @param ct parameter is a Type String and represents city
+     * @param sp parameter is a Type String and represents stateProvince 
+     * @param cou parameter is a Type String and represents country 
+     */
+  
 	public Address(String sn, String pc, String cq, String ct, String sp, String cou) throws IllegalArgumentException {
 		Pattern myPattern;
 		Matcher myMatcher;
@@ -83,6 +96,11 @@ public class Address {
 		}
 	}
 
+    /**
+     * Address constuctor, Initializes the private variables of
+     * streetName, postalCode, cityQuadrant, city, stateProvince, 
+     * and country with a string value of ""
+     */
 	public Address() {
 		streetName = "";
 		postalCode = "";
@@ -92,30 +110,61 @@ public class Address {
 		country = "";
 	}
 
+    /**
+     * setStreetName method is a setter meathod, the private variables of
+     * streetName is initilized with the value of a
+     * @param a, is a String type and is used to set the private variables of streetName
+     */
 	public void setStreetName(String a) {
 		streetName = a;
 	}
 
+    /**
+     * getStreetName method is a getter meathod, the private variables of
+     * streetName is returned as a String value
+     * @return
+     */
 	public String getStreetName() {
 		return streetName;
 	}
 
+    /**
+     * setPostalCode method is a setter meathod, the private variables of
+     * postalCode is initilized with the value of a
+     * @param a, is a String type and is used to set the private variables of postalCode
+     */
 	public void setPostalCode(String a) {
 		postalCode = a;
 	}
-
+    /**
+     * getPostalCode method is a getter meathod, the private variables of
+     * postalCode is returned as a String value
+     * @return
+     */
 	public String getPostalCode() {
 		return postalCode;
 	}
-
+    /**
+     * setCityQuadrant method is a setter meathod, the private variables of
+     * cityQuadrant is initilized with the value of a
+     * @param a, is a String type and is used to set the private variables of cityQuadrant
+     */
 	public void setCityQuadrant(String a) {
 		cityQuadrant = a;
 	}
-
+    /**
+     * getCityQuadrant method is a getter meathod, the private variables of
+     * cityQuadrant is returned as a String value
+     * @return
+     */
 	public String getCityQuadrant() {
 		return cityQuadrant;
 	}
-
+    /**
+     * setCity method is a setter meathod, the private variables of
+     * city is initilized with the value of a
+     * @param a, is a String type and is used to set the private variables of city
+     */
 	public void setCity(String a) {
 		city = a;
 	}
@@ -123,23 +172,45 @@ public class Address {
 	public String getCity() {
 		return city;
 	}
-
+    /**
+     * setStateProvince method is a setter meathod, the private variables of
+     * stateProvince is initilized with the value of a
+     * @param a, is a String type and is used to set the private variables of stateProvince
+     */
 	public void setStateProvince(String a) {
 		stateProvince = a;
 	}
 
+    /**
+     * getStateProvince method is a getter meathod, the private variables of
+     * stateProvince is returned as a String value
+     * @return
+     */
 	public String getStateProvince() {
 		return stateProvince;
 	}
-
+    /**
+     * setCountry method is a setter meathod, the private variables of
+     * country is initilized with the value of a
+     * @param a, is a String type and is used to set the private variables of country
+     */
 	public void setCountry(String a) {
 		country = a;
 	}
-
+    /**
+     * getCountry method is a getter meathod, the private variable of
+     * country is returned as a String value
+     * @return
+     */
 	public String getCountry() {
 		return country;
 	}
-
+	/**
+     * getAddressFormatted method is a getter meathod, the private variables of
+     * streetName + " " + cityQuadrant + ", " + city + " " + stateProvince + ", " + postalCode
+     * as a single large String value are returned
+     * @return
+     */
 	public String getAddressFormatted() {
 		return this.streetName + " " + this.cityQuadrant + ", " + this.city + " " + this.stateProvince + ", "
 				+ this.postalCode;
