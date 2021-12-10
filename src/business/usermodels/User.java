d1661f13b196c0f8a58694677d673b605b769cf3
ibrahim29asad <72579268+ibrahim29asad@users.noreply.github.com>
2021-12-10 13:39:51 -0700

    Add files via upload

--- src/business/usermodels/User.java
+++ src/business/usermodels/User.java
@@ -17,6 +17,18 @@ public class User {
 	private String lastName;
 	private String userType;
 
+
+email, password, firstName, lastName, userType
+
+	/**
+	 * User constuctor, Initializes the private variable values of email, password, firstName, lastName, userType
+	 * with the values email, password, firstName, lastName, userType that is passed through the constructor
+	 * @param email parameter is a Type String 
+	 * @param password parameter is a Type String 
+	 * @param firstName parameter is a Type String
+	 * @param lastName parameter is a Type String 
+	 * @param userType parameter is a Type String 
+	 */ 
 	public User(String email, String password, String firstName, String lastName, String userType) {
 		this.email = email;
 		this.password = password;
@@ -24,7 +36,11 @@ public class User {
 		this.lastName = lastName;
 		this.userType = userType;
 	}
-
+	
+	/**
+	 * User constuctor, Initializes the private variable values of email, password, firstName, lastName, userType
+	 * with the String value of ""
+	 */ 
 	public User() {
 		email = "";
 		password = "";
@@ -33,6 +49,15 @@ public class User {
 		userType = "";
 	}
 
+
+	/**
+     * toString method, has an Override and it formats a String as
+     * 
+     *  "- " + "Name: " + this.getFirstName() + " " + this.getLastName() + ", " + "	Email: " + this.getEmail()
+     * 
+     * and returns that string
+     * @return
+     */
 	@Override
 	public String toString() {
 		String s = "- ";
@@ -41,42 +66,94 @@ public class User {
 		return s;
 	}
 
+
+
+    /**
+     * setEmail method is a setter meathod, the private variables of
+     * email is initilized with the value of email that is passed into the function
+     * @param email, is a String type and is used to set the private variables of email
+     */
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
+	/**
+     * getEmail method is a getter meathod, the private variables of
+     * email is returned as a String value
+     * @return
+     */
 	public String getEmail() {
 		return email;
 	}
 
+    /**
+     * setPassword method is a setter meathod, the private variables of
+     * password is initilized with the value of password that is passed into the function
+     * @param password, is a String type and is used to set the private variables of password
+     */
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
+	
+	/**
+     * getPassword method is a getter meathod, the private variables of
+     * password is returned as a String value
+     * @return
+     */
 	public String getPassword() {
 		return password;
 	}
 
+    /**
+     * setFirstName method is a setter meathod, the private variables of
+     * firstName is initilized with the value of firstName that is passed into the function
+     * @param firstName, is a String type and is used to set the private variables of firstName
+     */
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
+	/**
+     * getFirstName method is a getter meathod, the private variables of
+     * firstName is returned as a String value
+     * @return
+     */
 	public String getFirstName() {
 		return firstName;
 	}
 
+    /**
+     * setLastName method is a setter meathod, the private variables of
+     * lastName is initilized with the value of lastName that is passed into the function
+     * @param lastName, is a String type and is used to set the private variables of lastName
+     */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
+	/**
+     * getLastName method is a getter meathod, the private variables of
+     * lastName is returned as a String value
+     * @return
+     */
 	public String getLastName() {
 		return lastName;
 	}
 
+    /**
+     * setUserType method is a setter meathod, the private variables of
+     * userType is initilized with the value of userType that is passed into the function
+     * @param userType, is a String type and is used to set the private variables of userType
+     */
 	public void setUserType(String userType) {
 		this.userType = userType;
 	}
 
+	/**
+     * getUserType method is a getter meathod, the private variables of
+     * userType is returned as a String value
+     * @return
+     */
 	public String getUserType() {
 		return userType;
 	}
