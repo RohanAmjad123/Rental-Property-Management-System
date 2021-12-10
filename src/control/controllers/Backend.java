43751f31e51827b23d08b968867096c9fcd9fc3a
NightWng <gibranakmal@hotmail.com>
2021-12-10 07:35:27 -0700

    Fully Documented Backend.java
    Also added class description comments at the start of ChangeFeesController.java

--- src/control/controllers/Backend.java
+++ src/control/controllers/Backend.java
@@ -14,16 +14,32 @@ import java.util.*;
  * @author Ibrahim Asad
  * @version 1.0
  */
+
+/**
+ *Backend Class
+ *Brings all controllers together
+ *used to link model to view through the actions and components of the Changing Fees ability
+ *main() function is in this class
+ */
 public class Backend {
     Frontend view;
     CompanyDatabase model;
+    
+    //ArrayList of controllers 
     ArrayList<Controller> controllers;
 
     /**
-     * 
+     * Backend default constructor
+     * initializes all controllers
+     * adds them to an arraylist of controllers
+     * no params
      */
     public Backend() {
+    	
+    	//initialize the view
         view = new Frontend();
+        
+        //passing in username and pass for sql database
         model = new CompanyDatabase("jdbc:mysql://localhost:3306/Rental_Property_Management_System", "test", "testpass123");
 
         SignupController signupController = new SignupController(view, model);
@@ -49,13 +65,30 @@ public class Backend {
         controllers.add(periodicalSummaryReportController);
     }
 
+    /**
+     * runApp method with no return type
+     * executes as the runner method
+     * this method shows the the front end starting screen which is the first thing the user sees
+     * no params
+     */
     public void runApp() {
         // set frontend view true
         view.setVisible(true);
     }
-
+    
+    
+    /**
+     * MAIN method with no return type
+     * this is where the program executes
+     * first method to run before anything runs and is the starting point    
+     * @param args[] isn't used
+     */
     public static void main(String args[]) {
+    	
+    	//Backend object initialized
         Backend e = new Backend();
+        
+        //app is started by calling runner through backend object
         e.runApp();
     }
 }
