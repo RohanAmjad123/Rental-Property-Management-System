03b7a0318ab3533259214c984b9d13d2cbdcfe82
AzlanAmjad <Azlanamjad_123@hotmail.com>
2021-12-10 17:12:07 -0700

    added Email Renter notification functionality

--- src/control/controllers/RegisterPropertyController.java
+++ src/control/controllers/RegisterPropertyController.java
@@ -5,9 +5,17 @@ import java.awt.event.ActionListener;
 import presentation.guicomponents.*;
 import datasource.companydatabase.*;
 import business.businesslogic.*;
+import business.businesslogic.Address;
 import business.usermodels.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Properties;
 import java.sql.SQLException;
+
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.activation.*;
+
 /**
  * Class RegisterPropertyController
  * @since December 4th, 2021
@@ -18,66 +26,70 @@ import java.sql.SQLException;
  * @version 1.0
  */
 
-
 /**
- *Controller Class for Register Property
- *implements the Controller class
- *implements the ActionListener class
- *used to link model to view through the actions and components of the process of Registering a Property
+ * Controller Class for Register Property
+ * implements the Controller class
+ * implements the ActionListener class
+ * used to link model to view through the actions and components of the process
+ * of Registering a Property
  */
 public class RegisterPropertyController implements Controller, ActionListener {
     Frontend view;
     CompanyDatabase model;
-    
+
     /**
      * Register Property Controller class constructor
      * executes as soon as an instance of this class is created
      * Action Listeners are binded to the different buttons in the different panes
-     * They will perform different functions on-click, specifically being able to: 
+     * They will perform different functions on-click, specifically being able to:
      * register property, try again, finish, and exit to dashboard
      * landlord only has access to this functionality
-     * @param view is the Frontend instance that all gui components are pulled from
-     * @param model is the Company Database instance with the data and the required information for the backend used to connect to SQL database
+     * 
+     * @param view  is the Frontend instance that all gui components are pulled from
+     * @param model is the Company Database instance with the data and the required
+     *              information for the backend used to connect to SQL database
      */
     public RegisterPropertyController(Frontend view, CompanyDatabase model) {
         this.view = view;
         this.model = model;
 
-        //relevant buttons in gui binded with action listeners
+        // relevant buttons in gui binded with action listeners
         view.getRegisterProperty().getRegisterPropertyButton().addActionListener(this);
         view.getRegisterProperty().getDashboardButton().addActionListener(this);
         view.getRegisterProperty().getTryAgainButton().addActionListener(this);
         view.getRegisterProperty().getFinishButton().addActionListener(this);
     }
 
-    
     /**
      * Action Performed method with no return type
      * executes in response to something triggereing the ActionListener
      * in this case, it can be any of the buttons binded with action listeners
-     * this method decides what action to take based off of which button was clicked e.g to register property, or exit to dash board
-     * @param e is the instance of the ActionEvent object which is the action event that triggers the ActionListener requiring handling
+     * this method decides what action to take based off of which button was clicked
+     * e.g to register property, or exit to dash board
+     * 
+     * @param e is the instance of the ActionEvent object which is the action event
+     *          that triggers the ActionListener requiring handling
      */
     public void actionPerformed(ActionEvent e) {
-    	
-    	// if 'x' (exit to dashboard) button is clicked
+
+        // if 'x' (exit to dashboard) button is clicked
         if (e.getSource() == view.getRegisterProperty().getDashboardButton()) {
             view.dashboard();
             view.getDashboard().loggedInLandlord();
         }
-        
-        //if finish button is pressed
+
+        // if finish button is pressed
         else if (e.getSource() == view.getRegisterProperty().getFinishButton()) {
             view.dashboard();
             view.getDashboard().loggedInLandlord();
         }
-        
-        //if try again button is pressed
+
+        // if try again button is pressed
         else if (e.getSource() == view.getRegisterProperty().getTryAgainButton()) {
             view.getRegisterProperty().registrationForm();
         }
-        
-        //if register property button is pressed
+
+        // if register property button is pressed
         else if (e.getSource() == view.getRegisterProperty().getRegisterPropertyButton()) {
             String title = "";
             String description = "";
@@ -96,7 +108,7 @@ public class RegisterPropertyController implements Controller, ActionListener {
             String month = String.valueOf(d.getMonthValue());
             String year = String.valueOf(d.getYear());
             DateModel listingDate = new DateModel(year, month, day);
-            
+
             String streetName = "";
             String postalCode = "";
             String cityQuadrant = "";
@@ -121,17 +133,15 @@ public class RegisterPropertyController implements Controller, ActionListener {
                 stateProvince = view.getRegisterProperty().getProvinceStateText().getText();
                 country = view.getRegisterProperty().getCountryText().getText();
 
-                //assign obtained values to local variables
+                // assign obtained values to local variables
                 rent = Integer.parseInt(rentString);
                 bedrooms = Integer.parseInt(bedroomsString);
                 bathrooms = Integer.parseInt(bathroomsString);
                 squareFeet = Integer.parseInt(squareFeetString);
-            }
-            catch(NullPointerException exception) {
+            } catch (NullPointerException exception) {
                 exception.printStackTrace();
                 view.getRegisterProperty().errorPage();
-            }
-            catch(NumberFormatException exception) {
+            } catch (NumberFormatException exception) {
                 exception.printStackTrace();
                 view.getRegisterProperty().errorPage();
             }
@@ -139,32 +149,84 @@ public class RegisterPropertyController implements Controller, ActionListener {
             try {
                 SingletonLogin x = SingletonLogin.getInstance();
                 User u = x.getCurrentUser();
-                if (u==null) {
+                if (u == null) {
                     throw new NullPointerException("Fatal System Error Aborting!");
                 }
                 landlordID = u.getEmail();
 
-               
                 address = new Address(streetName, postalCode, cityQuadrant, city, stateProvince, country);
-            
-                //new property initialized with stored specifications
-                Property p = new Property(title, description, propertyType, rent, bedrooms, bathrooms, squareFeet, furnished, landlordID, state, feeExpiry, feeAmount, listingDate, address);
-                            
+
+                // new property initialized with stored specifications
+                Property p = new Property(title, description, propertyType, rent, bedrooms, bathrooms, squareFeet,
+                        furnished, landlordID, state, feeExpiry, feeAmount, listingDate, address);
+
                 model.registerProperty(p);
                 view.getRegisterProperty().successPage();
-              
-            }
-            catch (NullPointerException exception) {
+
+                // notify renters of newly registered property
+                notifyRenters(model.getRentersWIthSubscriptionsMatching(p));
+
+            } catch (NullPointerException exception) {
                 exception.printStackTrace();
                 System.exit(1);
-            }
-            catch (IllegalArgumentException exception) {
+            } catch (IllegalArgumentException exception) {
+                exception.printStackTrace();
+                view.getRegisterProperty().errorPage();
+            } catch (SQLException exception) {
                 exception.printStackTrace();
                 view.getRegisterProperty().errorPage();
             }
-            catch (SQLException exception) {
-                exception.printStackTrace();
-                view.getRegisterProperty().errorPage();
+        }
+    }
+
+    /**
+     * Notifies renters of newly registered property
+     * 
+     * @param emails ArrayList<String> renter emails
+     */
+    public void notifyRenters(ArrayList<String> emails) {
+        // MAIL SERVICE IMPLEMENTATION
+
+        // from email address
+        String from = "comapany@mail.com";
+
+        // sending mail from local host
+        String host = "localhost";
+
+        // System properties
+        Properties properties = System.getProperties();
+
+        // setup mail server using properties
+        properties.setProperty("mail.smtp.host", host);
+
+        // session object
+        Session session = Session.getDefaultInstance(properties);
+
+        for (String to : emails) {
+            try {
+                // Mime Message Object
+                MimeMessage message = new MimeMessage(session);
+
+                // set From header field
+                message.setFrom(new InternetAddress(from));
+
+                // set To header field
+                message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+                // set Email subject line
+                message.setSubject("Email from RentSlower, New Property Matching Your Subscription!");
+
+                // set Email text from JTextArea
+                message.setText("A new property matching your Subscription on RentSlower was just registered, go check it out!");
+
+                // send email
+                Transport.send(message);
+            }
+            catch (NullPointerException exc) {
+                exc.printStackTrace();
+            }
+            catch (MessagingException exc) {
+                exc.printStackTrace();
             }
         }
     }
